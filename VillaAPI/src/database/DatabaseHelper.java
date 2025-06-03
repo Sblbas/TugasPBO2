8a6c74ff99b4bc45d8810df13add7e44aa7c58b3
ler1304 <enzofarrel18@gmail.com>
2025-06-03 21:28:32 +0800

    database helper rubah 1

--- VillaAPI/src/database/DatabaseHelper.java
+++ VillaAPI/src/database/DatabaseHelper.java
@@ -1,5 +1,17 @@
 package database;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
-public class DatabaseHelper {
+    public class DatabaseHelper {
+        private static final String DB_URL = "jdbc:sqlite:vila.db";
+
+        public static Connection connect() throws SQLException {
+            return DriverManager.getConnection(DB_URL);
+        }
+    }
+
+
     
-}
+
+
