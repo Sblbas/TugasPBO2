ef4843b288857bb81cc747fd2dd14b92a97edde0
ler1304 <enzofarrel18@gmail.com>
2025-06-10 10:37:23 +0800

    nambah port di class main

--- VillaAPI/src/Main.java
+++ VillaAPI/src/Main.java
@@ -5,6 +5,8 @@ public class Main {
     private static final String API_KEY = "APIKEY123";
 
     public static void main(String[] args) {
+        port(8080);
+
         before((req, res) -> {
             String key = req.headers("x-api-key");
             if (!API_KEY.equals(key)) {
